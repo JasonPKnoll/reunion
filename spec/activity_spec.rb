f545a607014a890668f7c27814a99cc9b8653e74
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-07 20:59:04 -0700

    Add spec test for activity class

---
+++ spec/activity_spec.rb
@@ -0,0 +1,24 @@
+describe Activities do
+  before(each:) do
+    @activity = Activity.new("Brunch")
+  end
+
+  it 'exists' do
+    expect(@activity).to be_a(Activity)
+  end
+
+  it 'has attributes' do
+    expect(@activity.name).to eq("Brunch")
+    expect(@participants).to eq({})
+  end
+
+  it 'can add participants' do
+    @activity.add_participant("Maria", 20)
+    expect(@activity.participants).to eq({"Maria" => 20})
+    expect(@activity.costs).to eq(20)
+
+    @activity.add_participant("Luther", 40)
+    expect(@activity.participants).to eq({"Maria" => 20, "Luther" => 40})
+    expect(@activity.costs).to eq(60)
+  end
+end
