31b4e98c570f29f8b137f6ec3dea129ea23fb6dd
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-08 10:30:08 -0700

    Finished iteration3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,38 @@ class Reunion
     activites << new_activity
   end
 
+  def total_cost
+    activites.map do |activity|
+      activity.total_cost
+    end.sum
+  end
+  # BREAKOUT
+  # activities inside of the reunion
+  # need totals that each participant has from each activity
+
+  def breakout
+    # total_hash = Hash.new(0) # the zero in () sets the default value to 0
+    # Hash.new { |hash, key| hash[key] = 0 }
+    # activites.each do |activity|
+    #   activity.owed.each do |name, amount|
+    #     total_hash[name] += amount
+    total_hash =  Hash.new(0)
+    activites.map do |activity|
+      difference = (activity.total_cost / activity.participants.count)
+      activity.participants.each do |name, cost|
+        costs = difference - cost
+        (total_hash[name] += costs)
+      end
+    end
+    total_hash.merge
+  end
+
+  def summary
+    summary = ""
+    # breakout #{"Maria"=>-10, "Luther"=>-30, "Louis"=>40}
+    breakout.each do |name, amount|
+      summary += "#{name.to_s}: #{amount.to_s}\n"
+    end
+    summary.chomp # takes off the last \n
+  end
 end
--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -1,11 +1,13 @@
 require 'reunion'
+require 'activity'
 
 describe Reunion do
   before(:each) do
     @reunion = Reunion.new("1406 BE")
     @activity_1 = Activity.new("Brunch")
+    @activity_2 = Activity.new("Drinks")
   end
-  it 'Exists' do
+  it 'exists' do
     expect(@reunion).to be_a(Reunion)
   end
 
@@ -17,5 +19,32 @@ describe Reunion do
   it ' can add activities' do
     @reunion.add_activity(@activity_1)
     expect(@reunion.activites).to eq([@activity_1])
+  end
+
+  it 'Can give the total costs for all added activites' do
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(@activity_1)
+
+    expect(@reunion.total_cost).to eq(60)
+
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(@activity_2)
+
+    expect(@reunion.total_cost).to eq(180)
+  end
+
+  it 'return who owes how much money from all activites' do
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(@activity_1)
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(@activity_2)
+    expect(@reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
+    expect(@reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
   end
 end
